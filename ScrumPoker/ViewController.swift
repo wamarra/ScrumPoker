df26abb7690d53c2c8a3093e08be55c3ca0b3b11
Wesley Marra <cast.wesleya.utic@sebrae.com.br>
2021-11-02 06:09:44 -0300

    Initial Commit

---
+++ ScrumPoker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrumPoker
+//
+//  Created by Wesley Marra on 02/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
